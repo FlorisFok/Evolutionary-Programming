8a1bb8aa1389121e62ab34f9422b1a5bc673e840
florisfok <florisfok5@gmail.com>
2019-10-01 11:26:00 +0200

    'jaaa'

--- Evoman_Floris3.py
+++ Evoman_Floris3.py
@@ -4,12 +4,17 @@ import sys
 from scipy import stats
 import datetime
 import time
+import sys
+import os
 sys.path.insert(0, 'evoman')
+
 from environment import Environment
 from demo_controller import player_controller, enemy_controller
+
+
 # initializes simulation for coevolution evolution mode.
 env = Environment(experiment_name="test",
-               enemies=[2],
+               enemies=[7],
                playermode="ai",
                player_controller=player_controller(),
                enemy_controller=enemy_controller(),
@@ -24,7 +29,7 @@ dom_l = -1
 env.update_parameter('contacthurt','player')
 
 # User dependent
-file_loc = r"C:\Users\FlorisFok\Documents\Master\Evo Pro\evoman_framework\individual_demo\evoman_log"
+file_loc = r"C:\Users\FlorisFok\Documents\Master\Evo Pro\evoman_framework\individual_demo"
 
 # evaluation
 def evaluate(x1):
@@ -74,14 +79,14 @@ def sex(parent1, parent2, scores):
     for i in range(NCHILDS):
         cross_prop = np.random.random()
 
-        if MODE == 'float':
+        if CROSS == 'fraction':
             # Crossover by float
             offspring = np.array(parent1[0])*cross_prop+np.array(parent2[0])*(1-cross_prop)
-        elif MODE == 'bit':
+        elif CROSS == 'parts':
             # Crossover by bits
             split_i = int(cross_prop*len(parent1))
             offspring = np.array(list(parent1[0])[:split_i] + list(parent2[0][split_i:]))
-        elif MODE == 'uniform':
+        elif CROSS == 'bits':
             # Crossover by bits
             offspring = np.zeros(parent1[0].shape)
             for i, a in enumerate(parent1[0]):
@@ -101,9 +106,17 @@ def mutate(individual, mutation_rate):
     Input: individual [np.array()], mutation rate [float]
     Output: mutated individual [np.array()]
     '''
-    for i in range(0,len(individual)):
-        if np.random.random() <= mutation_rate:
-            individual[i] = np.random.normal(0, 1)
+    if MUTATE == "random":
+        for i in range(0,len(individual)):
+            if np.random.random() <= mutation_rate:
+                individual[i] = np.random.normal(0, 1)
+
+    elif MUTATE == "noise":
+        # noise = np.random.normal(-1,1,len(individual)) * mutation_rate
+        # individual = individual + noise
+        for i in range(0,len(individual)):
+            if np.random.random() <= mutation_rate:
+                individual[i] += np.random.normal(-1, 1)
 
     individual = np.array(list(map(lambda y: limits(y), individual)))
     return individual
@@ -130,13 +143,27 @@ def parent_selection(all_pop):
     '''
     all_pop.sort(key=sortfit, reverse=True)
     pop_len = len(all_pop)
-    shindlerslist = survival(NPOP, pop_len)
-    cur_gen = [all_pop[i] for i in shindlerslist]
+
+    if SELECT == "Schindler":
+        shindlerslist = survival(NPOP, pop_len)
+        cur_gen = [all_pop[i] for i in shindlerslist]
+
+    elif SELECT == "prop":
+        indxs = list(range(len(all_pop)))
+        sum_fit = sum(np.array(all_pop)[:, 1] + 10)
+        prop = [ (i[1] + 10) / sum_fit for i in all_pop]
+        random_list = [np.random.choice(indxs, replace=False, p=prop) for i in range(NPOP)]
+        cur_gen = [all_pop[i] for i in random_list]
+        shindlerslist = 0
+
+    if LUCKY:
+        shindlerslist = indxs if shindlerslist == 0 else shindlerslist
+        lucky_list = [i for i in range(pop_len) if not i in shindlerslist and np.random.random() > 0.75]
+        lucky_gen = [all_pop[i] for i in lucky_list]
+    else:
+        lucky_gen = []
+
     np.random.shuffle(cur_gen)
-
-    lucky_list = [i for i in range(pop_len) if not i in shindlerslist and np.random.random() > 0.75]
-    lucky_gen = [all_pop[i] for i in lucky_list]
-
     return np.array(cur_gen), np.array(lucky_gen)
 
 def mutation_rate_change(mu):
@@ -145,32 +172,94 @@ def mutation_rate_change(mu):
     Input: mu [float]
     Ouput: mu [float]
     '''
-    return mu - delta_mu
+    # return mu - delta_mu
+    return mu
 
-def plot_scores(best_scores, avg_scores):
-    plt.figure(figsize=(12,12))
+def plot_scores(best_scores, avg_scores, namefig='noname'):
+    """
+    Plot results of the EA.
+    Plots:
+        - high
+        - low
+        - average
+    """
+    fig = plt.figure(figsize=(12,12))
+
     plt.plot(np.array(best_scores)[:,1], '.-', label="best", color='green')
     plt.plot(np.array(best_scores)[:,0], '.-', label="worst", color='red')
     plt.plot(avg_scores, linewidth=2, label="avg")
+
     plt.ylim((-10,100))
     plt.legend()
     plt.title("First run score")
-    plt.show()
+    fig.savefig(os.path.join(file_loc, name_check("evo_plot", '.png', file_loc)))
+
+    if PLOT:
+        plt.show()
+
 
 def save_log(inp, log, result):
-    loc = file_loc + str(inp) +".txt"
+    '''
+    Saves log to a txt file in the correct folder.
+    '''
+    loc = os.path.join(file_loc, name_check("evo_Log", ".txt", file_loc))
     with open(loc, "w") as f:
         f.write("LOG:\n" + str(log) + "\nResults: \n" + str(result))
 
+def get_settings():
+    arguments = ["NPOP", "GENS", "MU", "CROSS", "SELECT", "MUTATE", "LUCKY"]
+    settings = {"NPOP":None, "GENS":None,
+                "MU":None, "CROSS":None,
+                "SELECT":None, "MUTATE":None,
+                "LUCKY":None}
 
+    if len(sys.argv) >= 3:
+        for n, arg in enumerate(sys.argv[1:]):
+            if n < 3:
+                try:
+                    settings[arguments[n]] = float(arg)
+                except Exception as e:
+                    print(e)
+                    sys.exit()
+
+            elif n == 6:
+                settings[arguments[n]] = bool(arg)
+
+            else:
+                if not arg in ['bits', 'random', 'noise',
+                               'Schindler', 'fraction', 'parts']:
+                    print("Unknown setting")
+                    sys.exit()
+                settings[arguments[n]] = arg
+
+    else:
+        print("Usage: python evoman_floris.py NPOP GENS MU CROSS SELECT MUTATE LUCKY")
+
+    print(settings)
+    return settings
+
+def name_check(name, ext, file_loc):
+    i = 1
+    while name+str(i)+ext in os.listdir(file_loc):
+        i+=1
+
+    print(name+str(i)+ext)
+    return name+str(i)+ext
+
+settings = get_settings()
 
 # Algorithm settings
-NPOP = 20
-GENS = 20
-MU = 0.8
-delta_mu = MU/GENS
+NPOP = 20           if settings['NPOP'] == None else int(settings['NPOP'])
+GENS = 20           if settings['GENS'] == None else int(settings['GENS'])
+MU = 0.3            if settings['MU'] == None else settings['MU']
+CROSS = 'bits'      if settings['CROSS'] == None else settings['CROSS']
+SELECT = 'prop'   if settings['SELECT'] == None else settings['SELECT']
+MUTATE = 'random'    if settings['MUTATE'] == None else settings['MUTATE']
+LUCKY = True        if settings['LUCKY'] == None else settings['LUCKY']
+
+delta_mu = MU/GENS # MU gets to zero at last generation
 NCHILDS = 2
-MODE = 'uniform'
+PLOT = False
 
 ts = time.time()
 cur_gen = initialisatie(NPOP)
@@ -226,15 +315,19 @@ save_log("test", {
     "NPOP": NPOP,
     "GENS": GENS,
     "MUTA start": MU,
+    "Cross MODE": CROSS,
+    "Select Mode": SELECT,
+    "Mutate Mode": MUTATE,
+    "Lucky survivors": LUCKY,
     "MUTA step": delta_mu,
-    "NCHILDS": NCHILDS,
-    "Cross MODE": MODE,
+    "Number of Childs": NCHILDS,
     "time": datetime.date.today(),
     "total_time": total_time
 }, {
     "Best Score": best_scores[-1],
     "Scores": scores,
     "Avg Scores": avg_scores,
+    "Best Scores":best_scores,
     "sollution":sollution,
 })
-plot_scores(best_scores, avg_scores)
+plot_scores(best_scores, avg_scores, CROSS + SELECT + MUTATE + str(LUCKY))
